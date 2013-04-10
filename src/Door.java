7b612b17990ba6ec775055b5945e351997fb3176
MattHamersky <hamerskymatthew@yahoo.com>
2013-04-09 20:24:40 -0700

    Added getters and setters to  Door.java

--- src/Door.java
+++ src/Door.java
@@ -15,12 +15,31 @@
  */
 public class Door {
     int weight;
-    int from;
+	int from;
     int to;
+    
     public Door (int from,int to, int weight){
-	this.from = from;
-	this.to = to;
-	this.weight = weight;
+    	this.from = from;
+    	this.to = to;
+    	this.weight = weight;
     }
     
+    public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public int getFrom() {
+		return from;
+	}
+	public void setFrom(int from) {
+		this.from = from;
+	}
+	public int getTo() {
+		return to;
+	}
+	public void setTo(int to) {
+		this.to = to;
+	}
 }
